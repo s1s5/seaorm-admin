9d8dc81e660da3317d1643565dbd978df91e81b8
Shogo Sawai <shogo.sawai@gmail.com>
2023-06-29 16:50:06 +0900

    add unique to example entity

--- examples/migration/src/lib.rs
+++ examples/migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_create_table;
 mod m20230421_000833_add_test;
 mod m20230628_051557_add_tag;
+mod m20230629_020856_add_unique;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20230421_000833_add_test::Migration),
             Box::new(m20230628_051557_add_tag::Migration),
+            Box::new(m20230629_020856_add_unique::Migration),
         ]
     }
 }
